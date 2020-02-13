336cfc7e2c810231dbbf79eb1c876229fc601acd
Arkadiusz Zygan <a.zygan@gmail.com>
2020-02-13 18:50:39 +0100

    Add controllers to routes

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 // const ObjectId = require('mongodb').ObjectId;
-const Department = require('../models/department.model');
+const DepartmentController = require('../controllers/departments.controller');
+
+router.get('/departments', DepartmentController.getAll);
+router.get('/departments/random', DepartmentController.getRandom);
+router.get('/departments/:id', DepartmentController.getOne);
+router.post('/departments', DepartmentController.postAll);
+router.put('/departments/:id', DepartmentController.updateOne);
+router.delete('/departments/:id', DepartmentController.deleteOne);
+
+
 
 // router.get('/departments', (req, res) => {
 //   res.json(db.departments);
@@ -14,14 +23,14 @@ const Department = require('../models/department.model');
 //   });
 // });
 /* Mongoose */
-router.get('/departments', async (req, res) => {
-  try {
-    res.json(await Department.find());
-  }
-  catch(err) {
-    res.status(500).json(err)
-  }
-});
+// router.get('/departments', async (req, res) => {
+//   try {
+//     res.json(await Department.find());
+//   }
+//   catch(err) {
+//     res.status(500).json(err)
+//   }
+// });
 
 // router.get('/departments/random', (req, res) => {
 //   res.json(db.departments[Math.floor(Math.random() * db.length)]);
@@ -34,20 +43,20 @@ router.get('/departments', async (req, res) => {
 //   });
 // });
 /* Mongoose */
-router.get('/departments/random', async (req, res) => {
+// router.get('/departments/random', async (req, res) => {
 
-  try {
-    const count = await Department.countDocuments();
-    const rand = Math.floor(Math.random() * count);
-    const dep = await Department.findOne().skip(rand);
-    if(!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
-  }
-  catch(err) {
-    res.json(err);
-  }
+//   try {
+//     const count = await Department.countDocuments();
+//     const rand = Math.floor(Math.random() * count);
+//     const dep = await Department.findOne().skip(rand);
+//     if(!dep) res.status(404).json({ message: 'Not found' });
+//     else res.json(dep);
+//   }
+//   catch(err) {
+//     res.json(err);
+//   }
 
-});
+// });
 
 // router.get('/departments/:id', (req, res) => {
 //   res.json(db.departments.find(item => item.id == req.params.id));
@@ -61,18 +70,18 @@ router.get('/departments/random', async (req, res) => {
 //   });
 // });
 /* Mongoose */
-router.get('/departments/:id', async (req, res) => {
+// router.get('/departments/:id', async (req, res) => {
 
-  try {
-    const dep = await Department.findById(req.params.id);
-    if(!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
-  }
-  catch(err) {
-    res.status(500).json(err);
-  }
+//   try {
+//     const dep = await Department.findById(req.params.id);
+//     if(!dep) res.status(404).json({ message: 'Not found' });
+//     else res.json(dep);
+//   }
+//   catch(err) {
+//     res.status(500).json(err);
+//   }
 
-});
+// });
 
 // router.post('/departments', (req, res) => {
 //   const { name } = req.body;
@@ -88,20 +97,20 @@ router.get('/departments/:id', async (req, res) => {
 //   })
 // });
 /* Mongoose */
-router.post('/departments', async (req, res) => {
+// router.post('/departments', async (req, res) => {
 
-  try {
+//   try {
 
-    const { name } = req.body;
-    const newDepartment = new Department({ name: name });
-    await newDepartment.save();
-    res.json({ message: 'OK' });
+//     const { name } = req.body;
+//     const newDepartment = new Department({ name: name });
+//     await newDepartment.save();
+//     res.json({ message: 'OK' });
 
-  } catch(err) {
-    res.status(500).json(err);
-  }
+//   } catch(err) {
+//     res.status(500).json(err);
+//   }
 
-});
+// });
 
 
 // router.put('/departments/:id', (req, res) => {
@@ -118,23 +127,23 @@ router.post('/departments', async (req, res) => {
 //   });
 // });
 /* Mongoose */
-router.put('/departments/:id', async (req, res) => {
-  const { name } = req.body;
+// router.put('/departments/:id', async (req, res) => {
+//   const { name } = req.body;
 
-  try {
-    const dep = await(Department.findById(req.params.id));
-    if(dep) {
-      // await Department.updateOne({ _id: req.params.id }, { $set: { name: name }});
-      await Department.findOneAndUpdate({ _id: req.params.id }, { $set: { name: name }});
-      res.json({ message: 'OK' });
-    }
-    else res.status(404).json({ message: 'Not found...' });
-  }
-  catch(err) {
-    res.status(500).json(err);
-  }
+//   try {
+//     const dep = await(Department.findById(req.params.id));
+//     if(dep) {
+//       // await Department.updateOne({ _id: req.params.id }, { $set: { name: name }});
+//       await Department.findOneAndUpdate({ _id: req.params.id }, { $set: { name: name }});
+//       res.json({ message: 'OK' });
+//     }
+//     else res.status(404).json({ message: 'Not found...' });
+//   }
+//   catch(err) {
+//     res.status(500).json(err);
+//   }
 
-});
+// });
 
 // router.delete('/departments/:id', (req, res) => {
 //   db = db.departments.filter(item => item.id != req.params.id)
@@ -148,21 +157,21 @@ router.put('/departments/:id', async (req, res) => {
 //   });
 // });
 /* Mongoose */
-router.delete('/departments/:id', async (req, res) => {
+// router.delete('/departments/:id', async (req, res) => {
 
-  try {
-    const dep = await(Department.findById(req.params.id));
-    if(dep) {
-      // await Department.deleteOne({ _id: req.params.id });
-      await Department.findOneAndDelete({ _id: req.params.id });
-      res.json({ message: 'OK' });
-    }
-    else res.status(404).json({ message: 'Not found...' });
-  }
-  catch(err) {
-    res.status(500).json(err);
-  }
+//   try {
+//     const dep = await(Department.findById(req.params.id));
+//     if(dep) {
+//       // await Department.deleteOne({ _id: req.params.id });
+//       await Department.findOneAndDelete({ _id: req.params.id });
+//       res.json({ message: 'OK' });
+//     }
+//     else res.status(404).json({ message: 'Not found...' });
+//   }
+//   catch(err) {
+//     res.status(500).json(err);
+//   }
 
-});
+// });
 
 module.exports = router;
